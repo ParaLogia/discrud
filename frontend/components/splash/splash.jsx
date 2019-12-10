bdf4c0a812acb0c730b76fe340a591f6c3c2a609
Phillip Huang <phillip96huang@gmail.com>
2019-12-10 16:11:45 -0500

    Complete essential splash page with styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Splash = ({ loggedIn }) => {
   const appLink = loggedIn ? (
-    <Link to="/channels/@me">Open</Link>
+    <Link to="/channels/@me" className="button">
+      Open
+    </Link>
   ) : (
-    <Link to="/login">Login</Link>
+    <Link to="/login" className="button">
+      Login
+    </Link>
   )
   return (
     <div className="splash">
@@ -39,6 +43,24 @@ const Splash = ({ loggedIn }) => {
           </li>
         </ul>
       </nav>
+      <section className="splash-content">
+        <h1>It&apos;s time to ditch Skype and Teamspeak.</h1>
+        <p>
+          All-in-one voice and text chat for gamers that&apos;s free, secure, and works on both your desktop and phone. Stop paying for TeamSpeak servers and hassling with Skype. Simplify your life.
+        </p>
+        <div className="splash-buttons-container">
+          <div className="splash-button-container">
+            <button className="download-button">
+              Download for your OS
+            </button>
+          </div>
+          <div className="splash-button-container">
+            <button className="open-app-button">
+              Open Discrud in your browser
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
