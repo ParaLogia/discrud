b036a8c9ad25748cd5429e911c972d779348f8b6
Phillip Huang <phillip96huang@gmail.com>
2019-12-12 10:30:04 -0500

    Add mini avatar to user footer

--- frontend/components/sidebar/user_footer.jsx
+++ frontend/components/sidebar/user_footer.jsx
@@ -6,9 +6,17 @@ const UserFooter = ({ currentUser, logout }) => {
     logout();
   }
 
+  const avatarIcon = (
+    <div className="avatar-mini">
+      {currentUser.username[0]}
+    </div>
+  )
+
   return (
     <footer className="user-footer">
+      {avatarIcon}
       <div className="username-container">
+
         <strong className="username">
           {currentUser.username}
         </strong>
